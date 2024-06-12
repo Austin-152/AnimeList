4d626c0b07e29717874b3f8d038b3c589270394f
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2024-06-12 08:23:02 +0800

    remove all

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+// src/pages/index.tsx
+import React from 'react';
+import { index as IndexComponent } from '../components/index';
+import 'tailwindcss/tailwind.css';
+
+const HomePage = () => (
+  <div>
+    <IndexComponent />
+  </div>
+);
+
+export default HomePage;
