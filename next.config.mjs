bcb4b8abe1e043e00f2855036afce237bf2f26b8
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2024-09-02 16:52:31 +0800

    update env

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
+import {randomBytes} from "node:crypto";
 
-// 手动加载 .env 文件
 dotenv.config();
+
 const nextConfig = {
   // load env
     env: {
         BaseURL: process.env.NEXT_PUBLIC_BASE_URL,
         LogtoCallback: (process.env.LOGTO_CALLBACK_URL || 'https://anime.tzpro.xyz'),
+        LogtoAppId: (process.env.LOGTO_APP_ID || '607i5yfqohw99j66f1p3a'),
+        LogtoAppSecret: (process.env.LOGTO_APP_SECRET || '9zXP71NQN3D8goUXqHpG6eIyo9aFr7yn'),
+        LogtoEndpoint: (process.env.LOGTO_ENDPOINT || 'https://auth.tzpro.xyz/oidc/auth'),
+        CookieSecret: (process.env.COOKIE_SECRET || randomBytes(32).toString('hex')),
     }
 }
 
--- src/lib/logto.js
+++ src/lib/logto.js
@@ -1,10 +1,12 @@
 import LogtoClient from '@logto/next';
+import dotenv from "dotenv";
+require('dotenv').config()
 
 export const logtoClient = new LogtoClient({
-    endpoint: 'https://auth.tzpro.xyz/oidc/auth',
-    appId: '607i5yfqohw99j66f1p3a',
-    appSecret: '9zXP71NQN3D8goUXqHpG6eIyo9aFr7yn',
-    baseUrl: 'https://anime.tzpro.xyz', // Change to your own base URL
-    cookieSecret: 'bCmw8bo3gtSLrEurCkHQ0aOYnmFs9iso', // Auto-generated 32 digit secret
-    cookieSecure: process.env.NODE_ENV === 'production',
+    endpoint: dotenv.config().parsed.LOGTO_ENDPOINT,
+    appId: dotenv.config().parsed.LOGTO_APP_ID,
+    appSecret: dotenv.config().parsed.LOGTO_APP_SECRET,
+    baseUrl: dotenv.config().parsed.BaseURL,
+    cookieSecret: process.env.COOKIE_SECRET,
+    cookieSecure: dotenv.config().parsed.NODE_ENV === 'production',
 });
