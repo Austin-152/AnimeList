7c2e2413f469d92fcdad97f94b929938c7d92448
Austin-152 <15118065377@163.com>
2024-09-02 20:58:26 +0800

    adjust nav on "Anime Hub" button's url

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -115,7 +115,7 @@ export default function Navbar() {
 
   return (
     <header className="bg-gray-950 text-gray-50 px-4 md:px-6 py-3 flex items-center justify-between">
-      <Link className="flex items-center gap-2 font-bold text-lg" href="#">
+      <Link className="flex items-center gap-2 font-bold text-lg" href="/">
         <FanIcon className="w-6 h-6" />
         <span>Anime Hub</span>
       </Link>
